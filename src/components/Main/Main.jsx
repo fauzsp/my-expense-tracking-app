ce3032d1bc168e6061a4653ef729d4af208eef23
Fauz Ali <fauz_sp@hotmail.com>
2022-03-07 17:42:09 +0500

    main compoennet created.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,10 +7,28 @@ import {
   Grid,
   Divider,
 } from "@material-ui/core";
+import useStyles from "./styles";
 const Main = () => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardHeader title="My expense tracking" subHeader="Expense tracker" />
+      <CardContent>
+        <Typography varient="h5" align="center">
+          Total Balance $100
+        </Typography>
+        <Typography varient="subtitle" style={{lineHeight: '1.5em', marginTop: '20px'}}>
+          Try saying Add income for $100 in category salary for Monday...
+        </Typography>
+        <Divider/>
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* LIST */}
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
