2a531b83706455338e7c4914546911c22b891095
fauzsp <fauz_sp@hotmail.com>
2022-03-10 08:35:05 +0500

    chartjs donut issue fixed by fixing undefined values

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -1,27 +1,42 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { ExpenseTrackerContext } from "./context/context";
-import { incomeCategories, expenseCategories, resetCategories } from './contants/category';
+
+import {
+  incomeCategories,
+  expenseCategories,
+  resetCategories,
+} from "./contants/category.js";
 
 const useTransactions = (title) => {
-    resetCategories();
-    debugger;
-    const {transactions} = useContext(ExpenseTrackerContext);
-    const transactionsPerType = transactions.filter((e) => e.type === title);
-    const total = transactionsPerType.reduce((accumulated, currentVal) => accumulated += currentVal.amount, 0); 
-    const categories = title === "Income" ? incomeCategories : expenseCategories;
-    console.log(transactionsPerType, total, categories, 'checking categories`')
-    transactionsPerType.forEach((t) => {
-        const category = categories.find((c) => c.type === t.category);
-        if(category) category.amount += t.amount;
-    });
-    const filteredCategories = categories.filter((e) => e.amount > 0);
-    const chartData = {
-    datasetes: [{
+  resetCategories();
+  const { transactions } = useContext(ExpenseTrackerContext);
+  debugger;
+  const rightTransactions = transactions.filter((t) => t.type === title);
+  const total = rightTransactions.reduce(
+    (acc, currVal) => (acc += currVal.amount),
+    0
+  );
+  const categories = title === "Income" ? incomeCategories : expenseCategories;
+
+  rightTransactions.forEach((t) => {
+    const category = categories.find((c) => c.type === t.category);
+
+    if (category) category.amount += t.amount;
+  });
+
+  const filteredCategories = categories.filter((sc) => sc.amount > 0);
+
+  const chartData = {
+    datasets: [
+      {
         data: filteredCategories.map((c) => c.amount),
-        backgroundColor: filteredCategories.map((c) => c.color)
-    }],
-    labels: filteredCategories.map((e) => e.type)
-}
-    return {filteredCategories, total, chartData}
-}
+        backgroundColor: filteredCategories.map((c) => c.color),
+      },
+    ],
+    labels: filteredCategories.map((c) => c.type),
+  };
+
+  return { filteredCategories, total, chartData };
+};
+
 export default useTransactions;
